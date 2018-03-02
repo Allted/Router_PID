e008e69c366ec763e75aef4ead257699feefa90d
Ryan Zellars <allted@gmail.com>
2018-03-01 21:50:13 -0800

    Jeff's more comments. US_PERIOD_TO_RPM is not working as 60 * 1000 * 1000 in rpm_math for some reason, works as a constant though?

--- Router_PID/Router_PID.ino
+++ Router_PID/Router_PID.ino
@@ -6,21 +6,22 @@
  * V1 Engineering
  */
 
-#include <PID_v1.h>                           // Arduino library PID Library by Brett Beauregard
+#include <PID_v1.h>                                  // Arduino library PID Library by Brett Beauregard
 
-const int PHOTO_PIN = 3;                      // Spindle IR Sensor input pin
-volatile unsigned long current_rpm_time = 0;  // Spindle RPM PWM input calc
-volatile unsigned long prev_rpm_time = 0;     // Spindle RPM PWM input calc
-volatile unsigned long rpm_value = 60000000;  // Spindle RPM input in microseconds
+const long US_PERIOD_TO_RPM = 60000000;              // Convert from us of one rotation to RPM.
 
-const int PWM_PIN = 2;                        // Marlin PWMms Input pin
-volatile unsigned long pwm_value = 0;         // Marlin PWMms value in microseconds
-volatile unsigned long prev_time = 0;         // Marlin PWMms Math
+const int PHOTO_PIN = 3;                             // Spindle IR Sensor input pin
+volatile unsigned long current_rpm_time = 0;         // Spindle RPM PWM input calc
+volatile unsigned long prev_rpm_time = 0;            // Spindle RPM PWM input calc
+volatile unsigned long rpm_value = US_PERIOD_TO_RPM; // Spindle RPM input in microseconds
 
-const int SPINDLE_ENABLE_PIN = 5;             // Marlin Spindle enabled pin 
-int spindleEnable = 0;                        // Marlin Spindle enabled value
+const int PWM_PIN = 2;                               // Marlin PWMms Input pin
+volatile unsigned long pwm_value = 0;                // Marlin PWMms value in microseconds
+volatile unsigned long prev_time = 0;                // Marlin PWMms Math
 
-const int ROUTER_PWM_OUTPIN = 11;             // Triac output to router
+const int SPINDLE_ENABLE_PIN = 5;                    // Marlin Spindle enabled pin 
+
+const int ROUTER_PWM_OUTPIN = 11;                    // Triac output to router
 
 // PID variables
 double Setpoint = 0.0;                           
@@ -34,7 +35,8 @@ double Kd=.215;
 
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);                   // PID library
 
-float MAX_TOOL_RPM = 30000;                                                  // SETTINGS per "spindle" ?1k headroom needed?
+const int MAX_TOOL_RPM = 30000;                                              // SETTINGS per "spindle" ?1k headroom needed?
+const int MAX_PWM_INPUT_US = 2024;                                           // Settings the microseconds of teh max PWM from Marlin.
 
 void setup()
 {
@@ -56,33 +58,43 @@ void setup()
 
 void loop() {
 
-   int spindle_enable = digitalRead(SPINDLE_ENABLE_PIN); // Marlin spindle power control
+   int spindle_enable = digitalRead(SPINDLE_ENABLE_PIN);     // Marlin spindle power control
     
-   unsigned long rpm_math = (60000000 / rpm_value) - 1;  // Spindle, interrupt microseconds to RPM
-   int optical_pwm = rpm_math * 255 / MAX_TOOL_RPM;      // Spindle, RPM to PWM
+   // Compute the spindle's RPM value.
+   unsigned long rpm_math = US_PERIOD_TO_RPM / rpm_value;    // Spindle, interrupt microseconds to RPM
+   unsigned int optical_pwm = rpm_math * 255 / MAX_TOOL_RPM; // Spindle, RPM to PWM
              
-   Input = optical_pwm;                                  // PID Input from router
-   Setpoint = map(pwm_value, 0, 2024, 0, 255);           // PID setpoit from Marlin
-   myPID.Compute();                                      // PID Run the Loop
+   // One iteration of the PID.
+   Input = optical_pwm;                                      // PID Input from router
+   Setpoint = map(pwm_value, 0, MAX_PWM_INPUT_US, 0, 255);   // PID setpoit from Marlin
+   myPID.Compute();                                          // PID Run the Loop
 
-   if (spindle_enable == 0){                             // Marlin is spindle off?
-       pwm_value = 0;                                    // Reset PID
-       analogWrite(ROUTER_PWM_OUTPIN, 0);                // Turn off Spindle AC
+   // Set the output of the speed controller.
+   if (spindle_enable == 0){                                 // Marlin is spindle off?
+       pwm_value = 0;                                        // Reset PID
+       analogWrite(ROUTER_PWM_OUTPIN, 0);                    // Turn off Spindle AC
        }
-    else {                                               // If spindle is enabled write PID value to triac
-       analogWrite(ROUTER_PWM_OUTPIN, Output);           // Out to AC control Triac
+    else {                                                   // If spindle is enabled write PID value to triac
+       analogWrite(ROUTER_PWM_OUTPIN, Output);               // Out to AC control Triac
        }
    
-      Serial.print("RPM = ");                            // Spindle, Display RPM
-      Serial.println(rpm_math);                          // Spindle, Display RPM
+      Serial.print("RPM = ");                                // Spindle, Display RPM
+      Serial.println(rpm_math);                              // Spindle, Display RPM
 }
 
 // spindleRPM gets called on a falling edge of the PHOTO_PIN, and records the amount of time
 // between edges in the rpm_value field (in microsecinds). ISr.
 void spindleRPM() {
+   // Capture the time at "now"
    current_rpm_time = micros();
+
+   // Store the microseconds since the last sample.
    rpm_value = current_rpm_time - prev_rpm_time;
+
+   // Start a new "timer" by setting the previous to "now".
    prev_rpm_time = current_rpm_time;
+
+   // Call this function on the next falling edge.
    attachInterrupt(digitalPinToInterrupt(PHOTO_PIN), spindleRPM, FALLING);
    }
 
